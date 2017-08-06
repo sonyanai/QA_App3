48ec976c5f6e12269cc99f8c459b498bcb1d43d7
sonyanai <taison0930@outlook.jp>
2017-08-06 18:33:42 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/taison/yanai/qa_app3/Answer.java
@@ -0,0 +1,38 @@
+package jp.techacademy.taison.yanai.qa_app3;
+
+/**
+ * Created by taiso on 2017/08/04.
+ */
+
+
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+    private String mBody;
+    private String mName;
+    private String mUid;
+    private String mAnswerUid;
+
+    public Answer(String body, String name, String uid, String answerUid) {
+        mBody = body;
+        mName = name;
+        mUid = uid;
+        mAnswerUid = answerUid;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getUid() {
+        return mUid;
+    }
+
+    public String getAnswerUid() {
+        return mAnswerUid;
+    }
+}
---
+++ app/src/main/java/jp/techacademy/taison/yanai/qa_app3/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.taison.yanai.qa_app3;
+
+/**
+ * Created by taiso on 2017/08/06.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
